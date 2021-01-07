93f8ec089698734b6e15501ad914baa04efd3209
Bazingamn <1804319795@qq.com>
2021-01-07 12:29:42 +0800

    sync

---
+++ com.dynamic.demo/com/dynamic/demo/com/dynamic/demo/CollectChange.java
@@ -0,0 +1,6 @@
+public class CollectChange {
+    
+    /**
+     * 动态规划问题，具有最优子结构
+     */
+}
--- com.dynamic.demo/com/dynamic/demo/com/dynamic/demo/fibo_StateTrainsition.java
+++ com.dynamic.demo/com/dynamic/demo/com/dynamic/demo/fibo_StateTrainsition.java
@@ -12,7 +12,7 @@ public class fibo_StateTrainsition {
     public int fib(int n){
         if (n == 1 || n == 2) {
             return 1;
-        }
+        } 
         int prev = 1, curr = 1;
         for (int i = 3; i <= n; i++) {
             int sum = prev + curr;
